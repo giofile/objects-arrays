0f23f5360f018eb58b0ca7a32697c8ccbb369fb3
giofile <77681874+giofile@users.noreply.github.com>
2021-07-21 17:39:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,103 @@
-const age = 18
-let name = "Brom";
-
-
-if (age >= 18 && age < 26){
- console.log("Je krijgt 50% korting!");
-} else {
-    console.log("volledige prijs")
+person ={
+    name: "Giovanni",
+    age: 47, 
+    evaluations: [7, 10, 9]
 }
+console.log(person);
+console.log(person.name);
+console.log(person.age);
+
+let testOne= person['name'];
+let testTwo = person['age'];
+
+console.log(testOne);
+console.log(testTwo);
+
+console.log(person.evaluations);
+// console.log(evaluations);
+
+// -----------------
+
+let testThree = ["groen", "rood", "blauw"]
+
+console.log(testThree);
+console.log(testThree.length);
+console.log(testThree[0]);
+const lastItem = testThree[testThree.length -1]
+console.log(lastItem)
 
 
-if( name === "Sarah" || name==="Bram"){
-    console.log("u ontvangt gratis een glaasje bier");
-} else{
-    console.log("volledige prijs");
-}
+testThree.push('geel')
+console.log(testThree);
+
+testThree.push('5')
+console.log(testThree);
+
+testThree.push({
+    greeting: "hi ik ben een object",
+    })
+
+    console.log(testThree[5]);
 
 
-let totalAmount = 100;
+console.log(testThree);
 
-if(totalAmount > 25 && totalAmount < 51 ){
-    console.log(" Free bitterballs");
-}else if (totalAmount >50  && totalAmount < 101 ){
-    console.log("Free portion of Nachos");
-}else if(totalAmount > 100 ){
-    console.log("Free bottle of champaign");
-}else{
-    console.log("geen extra's");
-}
+
+// ________________
+
+const catBreeds = [{
+    name: "Abyssinian",
+    description: "The Abyssinian is easy to care for, and a joy to have in your home. They’re affectionate cats and love both people and other animals.",
+    dog_friendly: 4,
+    energy_level: 5,
+    life_span: "14 - 15",
+    origin: "Egypt",
+    temperament: ["Active", "Energetic", "Independent", "Intelligent", "Gentle"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Abyssinian_(cat)",
+                food: {
+                        favourite_food: "fish",
+      medium_liked_food: "dried fruits", 
+      disliked_food: "walnuts"
+     }
+  },
+        {
+    name: "Aegean",
+    description: "Native to the Greek islands known as the Cyclades in the Aegean Sea, these are natural cats, meaning they developed without humans getting involved in their breeding. As a breed, Aegean Cats are rare, although they are numerous on their home islands. They are generally friendly toward people and can be excellent cats for families with children.",
+    dog_friendly: 4,
+    energy_level: 53,
+    life_span: "9- 12",
+    origin: "Greece",
+    temperament: [ "Affectionate", "Social", "Intelligent", "Playful", "Active"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/Aegean_cat",
+    food: {
+                        favourite_food: "tuna",
+      medium_liked_food: "canned food", 
+      disliked_food: "all fruits"
+     }
+  },
+        {
+    name: "American Bobtail",
+    description: "American Bobtails are loving and incredibly intelligent cats possessing a distinctive wild appearance. They are extremely interactive cats that bond with their human family with great devotion.",
+    dog_friendly: 5,
+    energy_level: 3,
+    life_span: "11 - 15",
+    origin: "United States",
+    temperament: [ "Intelligent", "Interactive", "Lively", "Playful", "Sensitive"],
+    wikipedia_url: "https://en.wikipedia.org/wiki/American_Bobtail",
+    food: {
+                        favourite_food: "meaty things",
+      medium_liked_food: "tuna", 
+      disliked_food: "canned food"
+     }
+  }
+]
+
+
+console.log(catBreeds[2].name);
+console.log(catBreeds[0].energy_level);
+console.log(catBreeds[1].temperament[0]);
+console.log(catBreeds[2].temperament[4]);
+console.log(catBreeds[2].food.favourite_food);
 
 
 
